6f0259c8e162f457b73a230e2b2b11ddc6f97d3d
ANTONYTHIVAKAR <antonythivakar@gmail.com>
2024-03-27 00:44:28 +0530

    movie listing

---
+++ src/app/movies/components/movie/movie.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+  @Input("movie") movie: Movie | undefined;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+
+  }
+
+  navigateToDetails(movieId?: number) {
+    if(movieId){
+      this.router.navigate(['/movies', movieId]);
+    }
+  }
+
+}
+
+
+
